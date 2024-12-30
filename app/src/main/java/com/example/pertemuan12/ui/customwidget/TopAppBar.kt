7493641d09f3298c30f5c93fa2e9a5c6e73da144
RamaSandhy <rshandy07@gmail.com>
2024-12-31 06:35:13 +0700

    Menambahkan CostumeTopAppBar sebagai komponen U

--- app/src/main/java/com/example/pertemuan12/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,17 @@
 package com.example.pertemuan12.ui.customwidget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    tille: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier= Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+){}
