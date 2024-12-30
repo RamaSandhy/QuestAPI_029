d8ed85c28e0bad88ea8129d2e251f5c0997a5e2a
RamaSandhy <rshandy07@gmail.com>
2024-12-31 06:20:53 +0700

    Menambahkan HomeUiState dan HomeViewModel untuk mengelola state Mahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/ViewModel/HomeViewModel.kt
@@ -1,2 +1,41 @@
 package com.example.pertemuan12.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.pertemuan12.Repository.MahasiswaRepository
+import com.example.pertemuan12.model.Mahasiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUlState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUlState = HomeUiState.Loading
+            mhsUlState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}
+
+
