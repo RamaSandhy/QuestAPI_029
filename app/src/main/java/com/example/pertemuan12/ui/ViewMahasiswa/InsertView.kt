c4c93b7264fd2e6e7d3734d2e4c457e4fd74254d
RamaSandhy <rshandy07@gmail.com>
2024-12-31 07:07:07 +0700

    Menambahkan DestinasiEntry sebagai implementasi DestinasiNavigasi

---
+++ app/src/main/java/com/example/pertemuan12/ui/ViewMahasiswa/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan12.ui.ViewMahasiswa
+
+import com.example.pertemuan12.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
